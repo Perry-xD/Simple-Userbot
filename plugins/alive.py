c2813c11eb38330ae464495bf467b76b36940193
GodBoyX <87893142+Perry-xD@users.noreply.github.com>
2021-11-03 20:41:33 +0530

    Update alive.py

--- plugins/alive.py
+++ plugins/alive.py
@@ -1,8 +1,8 @@
 from . import *
 
 from telethon import events, client
+from important import CMD_HELP
 
-CMD_HELP= {}
 @events.register(events.NewMessage(outgoing=True , pattern=r'\!jinda'))
 async def alive_me(event):
   await event.edit(f"**Aha Meh Alive {client.get_me(first_name)} Sir!**")
