07d7453d7d813391679b4730b0924ff3b408ed20
GodBoyX <87893142+Perry-xD@users.noreply.github.com>
2021-11-03 19:27:00 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 import os
 
 API_ID = int(os.environ.get("API_ID", 0)
-API_HASH = os.environ.get("API_HASH")
-BOT_TOKEN = os.environ.get("BOT_TOKEN")
-BOT_USERNAME = os.environ.get("BOT_USERNAME")
-SIMPLE_SESSION = os.environ.get("SIMPLE_SESSION")
-LOGGER_ID = os.environ.get("LOGGER_ID")
+API_HASH = os.environ.get("API_HASH", None)
+BOT_TOKEN = os.environ.get("BOT_TOKEN", None)
+BOT_USERNAME = os.environ.get("BOT_USERNAME", None)
+SIMPLE_SESSION = os.environ.get("SIMPLE_SESSION", None)
+LOGGER_ID = int(os.environ.get("LOGGER_ID", 0))
