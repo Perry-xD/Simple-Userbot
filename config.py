d903268b680c0301b730e994b33ea7f7cd56b2c3
GodBoyX <87893142+Perry-xD@users.noreply.github.com>
2021-11-03 18:27:16 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,8 @@
+import os
+
+API_ID = int(os.environ.get("API_ID", 0)
+API_HASH = os.environ.get("API_HASH")
+BOT_TOKEN = os.environ.get("BOT_TOKEN")
+BOT_USERNAME = os.environ.get("BOT_USERNAME")
+SIMPLE_SESSION = os.environ.get("SIMPLE_SESSION")
+LOGGER_ID = os.environ.get("LOGGER_ID")
