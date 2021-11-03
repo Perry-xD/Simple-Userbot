5fb909148819cdcc7638159dd1432eea35692191
GodBoyX <87893142+Perry-xD@users.noreply.github.com>
2021-11-03 20:36:20 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,7 +2,5 @@ import os
 
 API_ID = os.environ['API_ID']
 API_HASH = os.environ['API_HASH']
-BOT_TOKEN = os.environ['BOT_TOKEN']
-BOT_USERNAME = os.environ['BOT_USERNAME']
 SIMPLE_SESSION = os.environ['SIMPLE_SESSION']
-LOGGER_ID = os.environ['LOGGER_ID']
+
