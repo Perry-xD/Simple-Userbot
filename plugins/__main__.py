9439d67ab845a23d7553e41d250e3edd1f4f4547
GodBoyX <87893142+Perry-xD@users.noreply.github.com>
2021-11-03 20:53:56 +0530

    Create __main__.py

---
+++ plugins/__main__.py
@@ -0,0 +1,35 @@
+import plugins.alive, plugins.help
+import config
+import asyncio
+import logging
+
+from telethon import TelegramClient as tg
+from telethon.sessions import StringSession
+import glob
+import importlib
+import sys
+from pathlib import Path
+
+from telethon import events
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__==__main__:
+    simple.start()
+    simple.run_until_disconnected()
