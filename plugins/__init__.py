add769cfd2467e8c866a911c9a5e94e544b055aa
GodBoyX <87893142+Perry-xD@users.noreply.github.com>
2021-11-03 20:57:13 +0530

    Update __init__.py

--- plugins/__init__.py
+++ plugins/__init__.py
@@ -1,5 +1,8 @@
-from bot import *
+from telethon.sessions import StringSession
+from telethon import TelegramClient as tg
 from config import *
 from . import *
 
 CMD_HELP = {}
+
+simple = tg(StringSession(config.SIMPLE_SESSION), api_id=config.API_ID, api_hash=config.API_HASH)
