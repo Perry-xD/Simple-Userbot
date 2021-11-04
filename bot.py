97a900e1513722728d672fd65e1a35117416d8b3
GodBoyX <87893142+Perry-xD@users.noreply.github.com>
2021-11-04 15:14:59 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,24 +3,14 @@ import config
 import asyncio
 import logging
 
-from telethon import TelegramClient as tg
-from telethon.sessions import StringSession
-import glob
-import importlib
-import sys
-from pathlib import Path
 
+from plugins import simple
 from telethon import events
 
 
 
 
 logging.basicConfig(format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",level=logging.WARNING)
-
-
-simple = tg(StringSession(str(config.SIMPLE_SESSION)), api_id=config.API_ID, api_hash=config.API_HASH)
-
-
 
 
 
